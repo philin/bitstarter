6f5bad488df0195780531435fc6bb532aecbff0e
Philbert Lin <philin@gmail.com>
2013-07-11 23:08:36 +0000

    change path to index again

--- web.js
+++ web.js
@@ -4,12 +4,14 @@ var app = express.createServer(express.logger());
 
 var f = './index.html';
 var fileBuffer = fs.readFileSync(f);
+var theString = fileBuffer.toString();
 
 app.get('/', function(request, response) {
-  response.send(fileBuffer.toString());
+  response.send(theString);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
